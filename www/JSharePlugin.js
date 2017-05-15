005c0c089188494cdb07be36a03ef1485ba1d0fc
Hevin <liuhw@jpush.cn>
2017-05-15 13:58:53 +0800

    Add android libs

--- www/JSharePlugin.js
+++ www/JSharePlugin.js
@@ -2,7 +2,12 @@ var exec = require('cordova/exec')
 
 var JShare = function () {}
 
-
+function isAndroid () {
+  if (device.platform === 'Android') {
+    return true
+  }
+  return false
+}
 
 if (!window.plugins) {
   window.plugins = {}
